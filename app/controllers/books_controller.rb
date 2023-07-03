98efeb4d688d8212361bf2329e34811d1ea1e643
TaroDMM <dmm@gmail.com>
2023-07-03 11:14:23 +0000

    練習

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new
+    book.save
+    redirect_to 'book/show'
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
